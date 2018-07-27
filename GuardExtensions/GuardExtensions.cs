47c6ef33a261b190ff37bc56e1ce8a0658218c98
Andrew Boza <andrew.m.boza@gmail.com>
2018-07-27 17:46:14 -0400

    Adds xml documentation comments

--- GuardExtensions/GuardExtensions.cs
+++ GuardExtensions/GuardExtensions.cs
@@ -5,8 +5,16 @@ namespace GuardExtensions
 {
     public static class GuardExtensions
     {
+        /// <summary>
+        /// An extension method which throws an ArgumentNullException if the object is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="parameterName">The name of the parameter being checked. Hint: Use nameof(param)</param>
+        /// <returns>The same object if not null.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         [DebuggerStepThrough]
-        public static T ThrowIfNull<T>(this T obj, string parameterName)
+        public static T ThrowIfNull<T>(this T obj, string parameterName) where T : class
         {
             if (obj == null)
             {
@@ -16,6 +24,14 @@ namespace GuardExtensions
             return obj;
         }
 
+        /// <summary>
+        /// An extension method which throws an ArgumentNullException if the string is null or an ArgumentException if empty.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameterName">The name of the parameter being checked. Hint: Use nameof(param)</param>
+        /// <returns>The same object if not null or empty.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         [DebuggerStepThrough]
         public static string ThrowIfNullOrEmpty(this string obj, string parameterName)
         {
@@ -32,6 +48,14 @@ namespace GuardExtensions
             return obj;
         }
 
+        /// <summary>
+        /// An extension method which throws an ArgumentNullException if the string is null or an ArgumentException if empty or whitespace.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameterName">The name of the parameter being checked. Hint: Use nameof(param)</param>
+        /// <returns>The same object if not null, empty, or whitespace.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         [DebuggerStepThrough]
         public static string ThrowIfNullOrWhiteSpace(this string obj, string parameterName)
         {
